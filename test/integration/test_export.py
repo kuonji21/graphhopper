f41fa2afad287209a9156b6891293128570b3b87
kuonji21 <lancepaulmontemar88@gmail.com>
2025-05-10 23:02:48 +0800

    Added export feature tests

--- test/integration/test_export.py
+++ test/integration/test_export.py
@@ -1,1 +1,20 @@
 # Export feature tests (Lance) 
+from src.main import export_directions
+import os
+
+def test_export_directions():
+    orig = (200, "40.71", "-74.01", "New York")
+    dest = (200, "34.05", "-118.24", "Los Angeles")
+    fake_path_data = {
+        "paths": [{
+            "distance": 100000,
+            "time": 7200000,
+            "instructions": [{"text": "Turn left", "distance": 500}]
+        }]
+    }
+    export_directions(orig, dest, "car", fake_path_data)
+    filename = "directions_New_York_to_Los_Angeles.txt"
+    assert os.path.exists(filename)
+    # Cleanup
+    if os.path.exists(filename):
+        os.remove(filename)
